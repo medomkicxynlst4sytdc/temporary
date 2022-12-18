3a75912bddefeedcd3e2ea58ab379bfe19644ae1
medomkicxynlst4sytdc <120955131+medomkicxynlst4sytdc@users.noreply.github.com>
2022-12-19 02:05:14 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_twolip-userdebug
+lunch lineage_twolip-user
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka
 make bacon
